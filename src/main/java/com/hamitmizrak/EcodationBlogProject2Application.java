efb5e4da2ba7f4ac9def3c19ad56ebc1042695f2
hamitmizrak <hamitmizrak@gmail.com>
2022-06-19 15:54:03 +0300

    @SpringBootApplication ==> @EnableJpaAuditing

--- src/main/java/com/hamitmizrak/EcodationBlogProject2Application.java
+++ src/main/java/com/hamitmizrak/EcodationBlogProject2Application.java
@@ -2,12 +2,14 @@ package com.hamitmizrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 }
 )
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class EcodationBlogProject2Application {
 
     public static void main(String[] args) {
