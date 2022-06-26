131b3d212a6aa0b78af71f483af98602a40e339a
hamitmizrak <hamitmizrak@gmail.com>
2022-06-26 18:18:34 +0300

    Footer Component

---
+++ frontend/_7_react/blog/src/component/FooterComponent.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export default function FooterComponent() {
+    return (
+        <>
+            <footer className="text-center text-white" style={{
+                backgroundColor: 'black',
+               
+                }}>
+                <div className="container pt-4">
+                    <section className="mb-4">
+                        <a
+                            className="btn btn-link btn-floating btn-lg text-warning m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i className="fab fa-facebook-f"></i
+                        ></a>
+                        <a
+                            className="btn btn-link btn-floating btn-lg text-warning m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i className="fab fa-twitter"></i
+                        ></a>
+
+                        <a
+                            className="btn btn-link btn-floating btn-lg text-warning m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i className="fab fa-google"></i
+                        ></a>
+
+                        <a
+                            className="btn btn-link btn-floating btn-lg text-warning m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i className="fab fa-instagram"></i
+                        ></a>
+
+                        <a
+                            className="btn btn-link btn-floating btn-lg text-primary m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i className="fab fa-linkedin"></i
+                        ></a>
+
+                        <a
+                            className="btn btn-link btn-floating btn-lg text-warning m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i className="fab fa-github"></i
+                        ></a>
+                    </section>
+                </div>
+
+                <div className="text-center p-3" style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+                    color:"white"
+                    
+                    }}>
+                    © 2020 Copyright:
+                    <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                </div>
+            </footer>
+        </>
+    )
+}
--- frontend/_7_react/blog/src/component/HeaderComponent.jsx
+++ frontend/_7_react/blog/src/component/HeaderComponent.jsx
@@ -20,10 +20,13 @@ export default class HeaderComponent extends Component {
                             <div className="collapse navbar-collapse" id="collapsibleNavId">
                                 <ul className="navbar-nav me-auto mt-2 mt-lg-0">
                                     <li className="nav-item active">
-                                        <a className="nav-link" href="#">Home <span className="visually-hidden">(current)</span></a>
+                                        <a className="nav-link" href="#">{this.props.homepage} <span className="visually-hidden">(current)</span></a>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
+                                        <a className="nav-link" href="#">{this.props.contact}</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">{this.props.aboutme}</a>
                                     </li>
                                     <li className="nav-item dropdown">
                                         <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
@@ -33,7 +36,6 @@ export default class HeaderComponent extends Component {
                                         </div>
                                     </li>
                                 </ul>
-
                             </div>
                         </div>
                     </nav>
@@ -41,11 +43,13 @@ export default class HeaderComponent extends Component {
             </>
         )
     }
-}
+}//class ends
 
 //default props
 HeaderComponent.defaultProps={
     content:"blog projet",
     icon:"fa-brands fa-slack",
-    homepage:"Ana Sayfa"
+    homepage: "Ana Sayfa",
+    aboutme: "Hakkımızda",
+    contact: "İletişim",
 }
