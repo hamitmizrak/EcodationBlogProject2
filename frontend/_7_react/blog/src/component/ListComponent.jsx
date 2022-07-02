56a15fed259da9fd3e7d1ea26d5848a78db3d413
hamitmizrak <hamitmizrak@gmail.com>
2022-07-02 17:27:44 +0300

    tasarım-2

--- frontend/_7_react/blog/src/component/ListComponent.jsx
+++ frontend/_7_react/blog/src/component/ListComponent.jsx
@@ -5,13 +5,18 @@ import React, { Component } from 'react'
 // public String employeeEmail;
 
 export default class ListComponent extends Component {
+
+
     render() {
+        {/* TASARIM */ }
         return (
             <>
-
-            {/* TASARIM */}
                 <h1 className="text-center text-uppercase">List</h1>
                 <div className="row">
+                    <div className="mx-auto">
+                        <button className="btn btn-primary mb-3">EKLEME </button>
+                    </div>
+
                     <table className="table table-hover table-striped table-dark">
                         <thead>
                             <tr>
@@ -28,9 +33,16 @@ export default class ListComponent extends Component {
                                 <td></td>
                                 <td></td>
                                 <td></td>
-                                <td><i className="fa-solid fa-wrench text-primary"></i></td>
-                                <td><i className="fa-solid fa-street-view text-warning"></i></td>
-                                <td><i className="fa-solid fa-trash-can text-danger"></i></td>
+                                <td>
+                                    <button><i className="fa-solid fa-wrench text-primary"></i></button>
+                                </td>
+                                <td>
+                                    <button><i className="fa-solid fa-street-view text-warning"></i></button>
+                                </td>
+
+                                <td>
+                                    <button><i className="fa-solid fa-trash-can text-danger"></i></button>
+                                </td>
                             </tr>
                         </tbody>
 
