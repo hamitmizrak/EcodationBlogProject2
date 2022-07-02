ef27e2bdf54cc470c3604b72d8571a5a5240c256
hamitmizrak <hamitmizrak@gmail.com>
2022-07-02 15:19:10 +0300

    MainComponent.jsx

---
+++ frontend/_7_react/blog/src/component/MainComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainComponent extends Component {
+  render() {
+    return (
+      <>
+      MainComponent
+      </>
+    )
+  }
+}
