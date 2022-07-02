b8dfb4cfcf6271b0e46d8a2e5014c9b078d25576
hamitmizrak <hamitmizrak@gmail.com>
2022-07-02 15:18:55 +0300

    frontend revize

--- frontend/_7_react/blog/src/App.js
+++ frontend/_7_react/blog/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './component/HeaderComponent';
 import FooterComponent from './component/FooterComponent';
+import MainComponent from './component/MainComponent';
+
 
 function App() {
   return (
    <>
    <HeaderComponent/>
+   <MainComponent/>
    <FooterComponent/>
    </>
   );
--- frontend/_7_react/blog/src/component/ClassAndFunction.jsx
+++ frontend/_7_react/blog/src/component/ClassAndFunction.jsx
@@ -1,21 +1,18 @@
-// function componentte ==> render yoktur
-// class componentte    ==> render vardır
-import React, { Component } from 'react'
+// NOT: birden fazla metotla yapalım
+// kullanıcıdan alınan sayı kadar dizi elemanı olsun rastgele 1-10 arasındaki bu sayıları bir diziye atayalım
+// sonra bu diziyi başka bir diziye clone edelim.
+// clonelanmış bu dizi bu sayıları küçükten büyüğe doğru sıralayın (Arrays.sort())
+// en küçük sayıyı ve en büyük sayıyı bulalım ? ( diziyele yapalım)
+// bu sayıların toplamını ?
+// bu sayıların ortalaması ?
 
-export default class ClassAndFunction extends Component {
-  render() {
-    return (
-      <div>ClassAndFunction</div>
-    )
-  }
-}
+// bu dizideki elemanlardan tek sayılardan kaç tane ve tek sayı toplamları (tek sayı dizisi)
+// bu dizideki elemanlardan çift sayılardan kaç tane ve çift sayı toplamları(çift sayı dizisi)
+// dizilerde en büyük ile 25 karşılatıralım dizideki eleman 25 büyük mü
+// dizideki elemanlardan 5 sayısı var mı ? (java.util.Arrays.binarySearch())
+// dizideki tekrar eden sayıları bir dizide göstererelim ?
+// dizideki tekrar eden sayılar yerine 1 kere tekrar sayı göstersin (tekrarsız dizi elemanları)
 
-import React from 'react'
-export default function ClassAndFunction() {
-  return (
-    <div>ClassAndFunction</div>
-  )
-}
 
 
 
